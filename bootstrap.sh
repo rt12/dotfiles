a373d380df5f12c32809e0dee8bfe576a399febe
Dmitry Rakhalov <biorobot@gmail.com>
2017-12-18 10:33:24 +0300

    Added macos stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~
+
+if [ "$(uname)" == "Darwin" ]; then
+  cp -R macos/ ~
+fi
